1b344bff1e9d45cb7777faef65c9a872af0522f7
Xmart-xol <kulagiven@gmail.com>
2023-08-04 22:24:35 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>My App </h1>, document.getElementById('root'))
+ReactDOM.render(<h1>My App in react </h1>, document.getElementById('root'))
